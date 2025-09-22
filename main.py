b514efece3295f8a338b5bb6ffd1d7e7dbff7418
Kauan Maia <maiakauan613@icloud.com>
2025-09-22 05:39:17 -0300

    Adição do parêmetro skip_pending.

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from bot.handlers import start, actions
 
 if __name__ == "__main__":
     print("🤖 Bot está rodando...")
-    bot.infinity_polling(timeout=60, long_polling_timeout=55)
+    bot.infinity_polling(skip_pending=True, timeout=20, long_polling_timeout=15)
